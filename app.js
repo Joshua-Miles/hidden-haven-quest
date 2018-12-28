06b0b2be28413087f6d7d8df95694ea0891a38a6
Joshua Miles <joshua.miles@flatironschool.com>
2018-12-27 21:40:46 -0600

    init

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+var express = require('express');
+var app = express()
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+
+    console.log('socket connected')
+
+})
+
+app.use(express.static('public'))
+
+app.use('/deploy', function(req, res){
+    console.log('~DEPLOYING~')
+    const exec = require('child-process-promise').exec
+    exec('git pull origin automated-deployment')
+    res.send(200)
+})
+
+server.listen(80)
