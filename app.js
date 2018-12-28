f8d20ed6852755e97169f4b9e0a27381aebc7e73
Joshua Miles <joshua.miles@flatironschool.com>
2018-12-27 22:22:21 -0600

    fixed io issue

--- app.js
+++ app.js
@@ -6,35 +6,62 @@ const redirect = require('redirect-https');
 const app = express()
 
 const ssl = greenlock.create({
-    email: 'admin@triframe.io',
-    configDir: '~/.config/acme/',
-    agreeTos: true
-})
+    // Let's Encrypt v2 is ACME draft 11
+  version: 'draft-11'
 
-const httpServer = http.createServer(ssl.middleware(redirect()));
-const httpsServer = https.createServer(ssl.httpsOptions, ssl.middleware(app));
+  , server: 'https://acme-v02.api.letsencrypt.org/directory'
+    // Note: If at first you don't succeed, stop and switch to staging
+    // https://acme-staging-v02.api.letsencrypt.org/directory
+  
+    // You MUST change this to a valid email address
+  , email: 'joshua@christetulit.org'
+  
+    // You MUST NOT build clients that accept the ToS without asking the user
+  , agreeTos: true
+  
+    // You MUST change these to valid domains
+    // NOTE: all domains will validated and listed on the certificate
+  , approvedDomains: [ 'hidden-haven-quest.triframe.cloud' ]
+  
+    // You MUST have access to write to directory where certs are saved
+    // ex: /home/foouser/acme/etc
+  , configDir: '~/.config/acme/'
+  
+  , app: require('express')().use('/', function (req, res) {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.end('Hello, World!\n\n💚 🔒.js');
+    })
+  
+    // Get notified of important updates and help me make greenlock better
+  , communityMember: false
+  
+  , debug: true
+}).listen(80, 443);
 
-const io = require('socket.io')(httpsServer);
+// const httpServer = http.createServer(ssl.middleware(redirect()));
+// const httpsServer = https.createServer(ssl.httpsOptions, ssl.middleware(app));
 
-io.on('connection', function(socket){
+// const io = require('socket.io')(httpsServer);
 
-    console.log('socket connected')
+// io.on('connection', function(socket){
 
-})
+//     console.log('socket connected')
 
-app.use(express.static('public'))
+// })
 
-app.use('/deploy', function(req, res){
-    console.log('~DEPLOYING~')
-    const exec = require('child-process-promise').exec
-    exec('git pull origin master')
-    res.send(200)
-})
+// app.use(express.static('public'))
 
-httpServer.listen(80, function () {
-    console.log(`Listening on 80`)
-});
+// app.use('/deploy', function(req, res){
+//     console.log('~DEPLOYING~')
+//     const exec = require('child-process-promise').exec
+//     exec('git pull origin master')
+//     res.send(200)
+// })
 
-httpsServer.listen(443, () => {
-    console.log(`Listening on 443`)
-})
+// httpServer.listen(80, function () {
+//     console.log(`Listening on 80`)
+// });
+
+// httpsServer.listen(443, () => {
+//     console.log(`Listening on 443`)
+// })
