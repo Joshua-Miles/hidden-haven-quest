ceb84429304cace2e7bb81e7c26abb7c2e1a9b04
Joshua Miles <joshua.miles@flatironschool.com>
2018-12-28 07:54:26 -0600

    fixed io issue

---
+++ public/precache-manifest.10b8cfc64cc5a5ab579eb8e987350672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "292f39870461642efb9b",
+    "url": "/static/js/main.292f3987.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "292f39870461642efb9b",
+    "url": "/static/css/main.61add934.chunk.css"
+  },
+  {
+    "revision": "06614e35949503ceb4e8cd73009ea385",
+    "url": "/index.html"
+  }
+];
