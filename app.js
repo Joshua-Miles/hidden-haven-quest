94d19c08b1cec56d21f770509970bdbe238d8f1a
Joshua Miles <joshua.miles@flatironschool.com>
2018-12-27 22:01:26 -0600

    added ssl

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var express = require('express');
-var app = express()
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const express = require('express');
+const greenlock = require('greenlock-express');
+const https = require('https');
+const http = require('http');
+const redirect = require('redirect-https');
+const io = require('socket.io');
+
+const ssl = greenlock.create({
+    server: 'https://acme-v01.api.letsencrypt.org/directory',
+    email: 'admin@triframe.io',
+    agreeTos: true,
+    approvedDomains:[ "hidden-haven-quest.triframe.cloud" ]
+})
+
+const httpServer = http.createServer(ssl.middleware(redirect()));
+const httpsServer = https.createServer(ssl.httpsOptions, ssl.middleware(rest));
+
+const io = require('socket.io')(httpsServer);
 
 io.on('connection', function(socket){
 
@@ -18,4 +32,10 @@ app.use('/deploy', function(req, res){
     res.send(200)
 })
 
-server.listen(80)
+httpServer.listen(80, function () {
+    console.log(`Listening on 80`)
+});
+
+httpsServer.listen(443, () => {
+    console.log(`Listening on 443`)
+})
