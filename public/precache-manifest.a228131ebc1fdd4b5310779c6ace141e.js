9eea106a8571c58bf9bda81123bd70fe4292527c
Joshua Miles <joshua.miles@flatironschool.com>
2018-12-29 14:25:04 -0600

    things

---
+++ public/precache-manifest.a228131ebc1fdd4b5310779c6ace141e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "281d3a52c8bb245bb94c",
+    "url": "/static/js/main.281d3a52.chunk.js"
+  },
+  {
+    "revision": "90050aac580c718fdab2",
+    "url": "/static/js/1.90050aac.chunk.js"
+  },
+  {
+    "revision": "63774f6b630f22d4c4cf073ad9a825b8",
+    "url": "/index.html"
+  }
+];
