f4e4a3c25e332a063a7024f4ddded41e6e8b16f0
Joshua Miles <joshua.miles@flatironschool.com>
2018-12-29 16:41:01 -0600

    things

--- models/Rounds.js
+++ models/Rounds.js
@@ -56,7 +56,7 @@ class Rounds extends Model {
             let users = await Model.Users.all()
             users.forEach( user => {
                 client.messages.create({
-                    to: '+14044027002', //user.number,
+                    to: '+14044027007', //user.number,
                     from: '+12819496383',
                     body: `Hi ${user.name}, come play Hidden Haven Quest: https://hidden-haven-quest.triframe.cloud/portal/${user.id}`
                 });
